85ed5898d0dbd81cca58ceb8784a428cc90fd8b3
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2022-02-16 14:58:17 +0100

    Création de la classe Equipe

---
+++ src/main/java/com/montaury/mus/jeu/equipe/Equipe.java
@@ -0,0 +1,13 @@
+package com.montaury.mus.jeu.equipe;
+
+import com.montaury.mus.jeu.joueur.Joueur;
+
+public class Equipe {
+    Joueur joueur1;
+    Joueur joueur2;
+
+    public Equipe(Joueur joueur1, Joueur joueur2){
+        this.joueur1 = joueur1;
+        this.joueur2 = joueur2;
+    }
+}
