6c1f2cc8a49d23493ae25b3e61524591b7e1927a
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2022-02-16 15:01:32 +0100

    Changement des opposants dans le main 2 Joueurs -> 2 Equipes

--- src/main/java/com/montaury/mus/JeuDeMus.java
+++ src/main/java/com/montaury/mus/JeuDeMus.java
@@ -2,19 +2,23 @@ package com.montaury.mus;
 
 import com.montaury.mus.jeu.Partie;
 import com.montaury.mus.console.AffichageEvenements;
+import com.montaury.mus.jeu.equipe.Equipe;
 import com.montaury.mus.jeu.joueur.Joueur;
 import com.montaury.mus.jeu.Opposants;
 import java.util.Scanner;
 
 public class JeuDeMus {
-  public static void main(String[] args) {
-    System.out.print("Entrez votre nom: ");
-    var nomJoueur = new Scanner(System.in).next();
-    var joueurHumain = Joueur.humain(nomJoueur);
+    public static void main(String[] args) {
+        System.out.print("Entrez votre nom: ");
+        var nomJoueur = new Scanner(System.in).next();
+        var joueurHumain = Joueur.humain(nomJoueur);
 
-    var partie = new Partie(new AffichageEvenements(joueurHumain));
-    var resultat = partie.jouer(new Opposants(joueurHumain, Joueur.ordinateur()));
+        Equipe equipeHumainOrdinateur = new Equipe(joueurHumain, Joueur.ordinateur());
+        Equipe equipeOrdinateurOrdinateur = new Equipe(Joueur.ordinateur(), Joueur.ordinateur());
 
-    System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
-  }
+        var partie = new Partie(new AffichageEvenements(joueurHumain));
+        var resultat = partie.jouer(new Opposants(equipeHumainOrdinateur, equipeOrdinateurOrdinateur));
+
+        System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
+    }
 }
