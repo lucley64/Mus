2f41daab68b425ebf8f883fe4aab6af932324502
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2022-03-09 16:06:00 +0100

    Implémentation des équipes

--- src/main/java/com/montaury/mus/jeu/Opposants.java
+++ src/main/java/com/montaury/mus/jeu/Opposants.java
@@ -3,25 +3,27 @@ package com.montaury.mus.jeu;
 import com.montaury.mus.jeu.equipe.Equipe;
 import com.montaury.mus.jeu.joueur.Joueur;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.SynchronousQueue;
+import java.util.*;
 
 public class Opposants {
     private Equipe equipeEsku;
     private Equipe equipeZaku;
+    private List<Joueur> dansLOrdre;
 
     public Opposants(Equipe equipeEsku, Equipe equipeZaku) {
         this.equipeEsku = equipeEsku;
         this.equipeZaku = equipeZaku;
+        this.dansLOrdre = List.of(equipeEsku.joueur1(), equipeZaku.joueur1(),
+                equipeEsku.joueur2(), equipeZaku.joueur2());
     }
 
     public void tourner() {
-        var tmp = equipeEsku;
-        equipeEsku = equipeZaku;
-        equipeZaku = tmp;
+        List<Joueur> nouvelOrdre = new ArrayList<>();
+        nouvelOrdre.add(dansLOrdre.get(1));
+        nouvelOrdre.add(dansLOrdre.get(2));
+        nouvelOrdre.add(dansLOrdre.get(3));
+        nouvelOrdre.add(dansLOrdre.get(0));
+        dansLOrdre = nouvelOrdre;
     }
 
     public Equipe equipeEsku() {
@@ -33,7 +35,6 @@ public class Opposants {
     }
 
     public List<Joueur> dansLOrdre() {
-        return List.of(equipeEsku.joueur1(), equipeZaku.joueur1(),
-                equipeEsku.joueur2(), equipeZaku.joueur2());
+        return dansLOrdre;
     }
 }
