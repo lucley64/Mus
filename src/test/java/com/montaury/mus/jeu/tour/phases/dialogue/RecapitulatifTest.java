ecf94d90c64d65384ed5a32ecd24fabf3acb6d14
LEYDERT Luc <lleydert@iutbayonne.univ-pau.fr>
2022-03-09 16:33:57 +0100

    RecapitulatifTest passe

--- src/test/java/com/montaury/mus/jeu/tour/phases/dialogue/RecapitulatifTest.java
+++ src/test/java/com/montaury/mus/jeu/tour/phases/dialogue/RecapitulatifTest.java
@@ -21,74 +21,74 @@ class RecapitulatifTest {
 
     @Test
     void devrait_compter_1_point_engage_si_tous_les_joueurs_sont_paso() {
-//        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
-//                new Dialogue.ChoixJoueur(new Paso(), joueur1),
-//                new Dialogue.ChoixJoueur(new Paso(), joueur2))
-//        );
-//
-//        assertThat(dialogue.pointsEngages()).isEqualTo(1);
+        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
+                new Dialogue.ChoixJoueur(new Paso(), joueur1),
+                new Dialogue.ChoixJoueur(new Paso(), joueur2))
+        );
+
+        assertThat(dialogue.pointsEngages()).isEqualTo(1);
     }
 
     @Test
     void devrait_compter_1_point_engage_pour_imido_tira() {
-//        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
-//                new Dialogue.ChoixJoueur(new Imido(), joueur1),
-//                new Dialogue.ChoixJoueur(new Tira(), joueur2))
-//        );
-//
-//        assertThat(dialogue.pointsEngages()).isEqualTo(1);
+        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
+                new Dialogue.ChoixJoueur(new Imido(), joueur1),
+                new Dialogue.ChoixJoueur(new Tira(), joueur2))
+        );
+
+        assertThat(dialogue.pointsEngages()).isEqualTo(1);
     }
 
     @Test
     void devrait_compter_1_point_engage_pour_paso_imido_tira() {
-//        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
-//                new Dialogue.ChoixJoueur(new Paso(), joueur1),
-//                new Dialogue.ChoixJoueur(new Imido(), joueur2),
-//                new Dialogue.ChoixJoueur(new Tira(), joueur1))
-//        );
-//
-//        assertThat(dialogue.pointsEngages()).isEqualTo(1);
+        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
+                new Dialogue.ChoixJoueur(new Paso(), joueur1),
+                new Dialogue.ChoixJoueur(new Imido(), joueur2),
+                new Dialogue.ChoixJoueur(new Tira(), joueur1))
+        );
+
+        assertThat(dialogue.pointsEngages()).isEqualTo(1);
     }
 
     @Test
     void devrait_compter_1_point_engage_pour_3_imido_tira() {
-//        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
-//                new Dialogue.ChoixJoueur(new Imido(3), joueur1),
-//                new Dialogue.ChoixJoueur(new Tira(), joueur2))
-//        );
-//
-//        assertThat(dialogue.pointsEngages()).isEqualTo(1);
+        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
+                new Dialogue.ChoixJoueur(new Imido(3), joueur1),
+                new Dialogue.ChoixJoueur(new Tira(), joueur2))
+        );
+
+        assertThat(dialogue.pointsEngages()).isEqualTo(1);
     }
 
     @Test
     void devrait_compter_1_point_engage_pour_hordago_tira() {
-//        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
-//                new Dialogue.ChoixJoueur(new Hordago(), joueur1),
-//                new Dialogue.ChoixJoueur(new Tira(), joueur2))
-//        );
-//
-//        assertThat(dialogue.pointsEngages()).isEqualTo(1);
+        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
+                new Dialogue.ChoixJoueur(new Hordago(), joueur1),
+                new Dialogue.ChoixJoueur(new Tira(), joueur2))
+        );
+
+        assertThat(dialogue.pointsEngages()).isEqualTo(1);
     }
 
     @Test
     void devrait_compter_2_points_engages_pour_imido_idoki() {
-//        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
-//                new Dialogue.ChoixJoueur(new Imido(), joueur1),
-//                new Dialogue.ChoixJoueur(new Idoki(), joueur2))
-//        );
-//
-//        assertThat(dialogue.pointsEngages()).isEqualTo(2);
+        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
+                new Dialogue.ChoixJoueur(new Imido(), joueur1),
+                new Dialogue.ChoixJoueur(new Idoki(), joueur2))
+        );
+
+        assertThat(dialogue.pointsEngages()).isEqualTo(2);
     }
 
     @Test
     void devrait_compter_2_points_engages_pour_imido_hordago_tira() {
-//        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
-//                new Dialogue.ChoixJoueur(new Imido(), joueur1),
-//                new Dialogue.ChoixJoueur(new Hordago(), joueur2),
-//                new Dialogue.ChoixJoueur(new Tira(), joueur1))
-//        );
-//
-//        assertThat(dialogue.pointsEngages()).isEqualTo(2);
+        Dialogue.Recapitulatif dialogue = new Dialogue.Recapitulatif(List.of(
+                new Dialogue.ChoixJoueur(new Imido(), joueur1),
+                new Dialogue.ChoixJoueur(new Hordago(), joueur2),
+                new Dialogue.ChoixJoueur(new Tira(), joueur1))
+        );
+
+        assertThat(dialogue.pointsEngages()).isEqualTo(2);
     }
 
 }
